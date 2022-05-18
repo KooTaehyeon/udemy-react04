8757659eda8862d27b432f77285dbe576dc0727b
Kth <vcro12123@naver.com>
2022-05-18 21:21:26 +0900

    mealItem,form 컴포넌트 생성

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './AvailableMeals.module.css';
-
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 //더미
 const DUMMY_MEALS = [
   {
@@ -31,11 +32,20 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meals) => {
-    return <li key={meals.id}>{meals.name}</li>;
+    return (
+      <MealItem
+        key={meals.id}
+        name={meals.name}
+        description={meals.description}
+        price={meals.price}
+      />
+    );
   });
   return (
     <section className={styles.meals}>
-      <ul>{mealsList}</ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
   );
 };
