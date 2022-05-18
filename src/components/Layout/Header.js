30a3992b0d91f03a17ab2af352928058814a1f85
Kth <vcro12123@naver.com>
2022-05-18 19:10:36 +0900

    meals 컴포넌트 생성

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import meals from '../assets/meals.jpg';
+import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 const Header = (props) => {
-  return <div></div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>EeactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={styles['main-image']}>
+        <img src={meals} />
+      </div>
+    </>
+  );
 };
 
 export default Header;
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Meals.module.css';
+import AvailableMeals from './AvailableMeals';
+import MealsSummary from './MealsSummary';
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvailableMeals />
+    </>
+  );
+};
+
+export default Meals;
